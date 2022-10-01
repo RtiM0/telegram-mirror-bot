3f2beb1973a7ac0499674260f71640bba9b2e6f2
Mustafa Shakir <shakirmustafa58@gmail.com>
2022-10-01 08:40:34 +0530

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,172 @@
+import math
+import os
+import traceback
+from datetime import datetime, timedelta
+from re import findall
+from string import Template
+
+import asyncpraw
+from fake_headers import Headers
+from requests import get, head
+from telegram.constants import ParseMode
+from telegram.error import BadRequest
+from telegram.ext import Application, Defaults, PicklePersistence
+from yt_dlp import YoutubeDL
+from dotenv import load_dotenv
+
+from logger import logger
+from video import Video
+
+reddit = asyncpraw.Reddit('telegram mirror bot')
+
+REDDIT_COMMENT = Template('''
+[Mirror](https://soccer_mirror.t.me/$message_id/)
+''')
+
+def get_reddit(permalink="/r/soccer/new?limit=20") -> dict:
+        feed = get(
+            f"https://api.reddit.com{permalink}",
+            headers=Headers(headers=True).generate(),
+        ).json()
+        if type(feed) == list:
+            return feed[1]["data"]["children"]
+        else:
+            return feed["data"]["children"]
+
+def convert_size(size_bytes):
+   if size_bytes == 0:
+       return "0B"
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "%s %s" % (s, size_name[i])
+
+async def send_video(context, video, title, reddit_post, automod_id):
+    msg = await context.bot.send_video(
+        chat_id="@soccer_mirror",
+        video=video,
+        caption=f"{title}\n\n<a href=\"{reddit_post}\"><b>Reddit Link</b></a>",
+        supports_streaming=True,
+    )
+    try:
+        automod_comment = await reddit.comment(id=automod_id)
+        await automod_comment.reply(body=REDDIT_COMMENT.substitute(message_id=msg.message_id))
+    except Exception:
+        logger.error(traceback.format_exc())
+        pass
+
+async def look_for_goals(context):
+    context.bot_data["goalsdone"] = context.bot_data.get("goalsdone", [])
+    feed = get_reddit()
+    for post in feed:
+        post = post["data"]
+        title = post["title"]
+        if (
+            title not in context.bot_data["goalsdone"]
+            and post["url"] is not None
+            and post["link_flair_text"] is not None
+            and (
+                post["link_flair_text"].lower() == "media"
+                or post["link_flair_text"].lower() == "mirror"
+            )
+        ):
+            logger.info(f"FOUND GOAL: {title}")
+            ref = await get_stream([post["url"]], title, f"https://reddit.com{post['permalink']}", get_reddit(post["permalink"])[0]["data"]["id"], context)
+            if not ref:
+                context.job_queue.run_repeating(
+                    monitor_thread,
+                    60,
+                    data=[
+                        get_reddit(post["permalink"])[0]["data"][
+                            "permalink"
+                        ],
+                        [post["url"]],
+                        title,
+                        f"https://reddit.com{post['permalink']}",
+                        datetime.now() + timedelta(minutes=5),
+                    ],
+                    name=f"Monitor for {title}",
+                )
+            context.bot_data["goalsdone"].append(title)
+    
+
+async def get_stream(links: list, title: str, reddit_post: str, automod_id, context):
+    for link in links:
+        try:
+            with YoutubeDL({"logger": logger}) as ydl:
+                result = ydl.extract_info(link, download=False)
+                if "entries" in result:
+                    video = result["entries"][0]
+                else:
+                    video = result
+                try:
+                    if not video.get("url",None):
+                        continue
+                    vid_head = head(video["url"], allow_redirects=True)
+                    if (
+                        "youtube" not in video["url"]
+                        and vid_head.status_code == 200
+                    ):
+                        vid_size = int(vid_head.headers.get('content-length', 0))
+                        logger.info(f"Found direct for {link} [size - {convert_size(vid_size)}]")
+                        if (vid_size >= 14000000):
+                            logger.info(f"Compressing Video of size - {convert_size(vid_size)}")
+                            try:
+                                input_video = Video(vid_head.url)
+                                output_path = input_video.compress_video(12*1000)
+                                if output_path:
+                                    logger.info(f"Video compressed to size - {convert_size(output_path.stat().st_size)}")
+                                    await send_video(context, output_path.open("rb"), title, reddit_post, automod_id)
+                                    os.remove(output_path.resolve())
+                                    return True
+                                else:
+                                    continue
+                            except Exception:
+                                logger.error(traceback.format_exc())
+                                continue
+                        try:
+                            await send_video(context, vid_head.url,title, reddit_post, automod_id)
+                            return True
+                        except BadRequest:
+                            try:
+                                await send_video(context, get(vid_head.url).content,title, reddit_post, automod_id)
+                                return True
+                            except Exception:
+                                pass
+                except Exception:
+                    pass
+        except Exception:
+            pass
+    return False
+
+async def monitor_thread(context):
+    job = context.job
+    automoderator_permalink, links, title, reddit_post, end_time = job.data
+    automod = get_reddit(automoderator_permalink)[0]["data"]
+    mirrors_comments = automod["replies"]
+    if type(mirrors_comments) == dict:
+        for mirror in mirrors_comments["data"]["children"]:
+            urls = findall(
+                r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|%[0-9a-fA-F][0-9a-fA-F])+",
+                mirror["data"]["body"],
+            )
+            for url in urls:
+                links.append(url.strip("()"))
+    logger.info(f"Found {len(links)} mirrors for {title}")
+    ref = await get_stream(links, title, reddit_post, automod["id"], context)
+    if ref or (end_time < datetime.now()):
+        context.job.schedule_removal()
+
+def main():
+    load_dotenv()
+    application = Application.builder().token(os.environ.get("TOKEN")).defaults(defaults=Defaults(parse_mode=ParseMode.HTML)).read_timeout(300).persistence(persistence=PicklePersistence(filepath="tgmirrorbot")).build()
+    application.job_queue.run_repeating(
+        look_for_goals,
+        60,
+    )
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
---
+++ video.py
@@ -0,0 +1,112 @@
+import os
+from pathlib import Path
+from time import time
+from typing import Union
+from logger import logger
+
+import ffmpeg
+
+
+def current_milli_time():
+    return round(time() * 1000)
+
+class Video:
+    def __init__(self, video_path: str) -> None:
+        self.video_path: str = video_path
+        self.probe: dict = ffmpeg.probe(self.video_path)
+    
+    @property
+    def duration(self) -> float:
+        return float(self.probe['format']['duration'])
+
+    @property
+    def audio_bitrate(self) -> float:
+        return float(next((s for s in self.probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+
+    def compress_video(self, size_upper_bound: int, output_file_name: str = None, two_pass=True) -> Union[Path, None]:
+        """
+        Compress video file to max-supported size.
+        :param size_upper_bound: Max video size in KB.
+        :param output_file_name: Name of the output file.
+        :param two_pass: Set to True to enable two-pass calculation.
+
+        REFERENCE: https://gist.github.com/ESWZY/a420a308d3118f21274a0bc3a6feb1ff
+        """
+
+        video_full_path = self.video_path
+        file_name = output_file_name if output_file_name else f"{current_milli_time()}.mp4"
+
+        # Adjust them to meet your minimum requirements (in bps), or maybe this function will refuse your video!
+        total_bitrate_lower_bound = 11000
+        min_audio_bitrate = 32000
+        max_audio_bitrate = 256000
+        min_video_bitrate = 100000
+
+        try:
+            # Bitrate reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+            if output_file_name:
+                self.probe = ffmpeg.probe(output_file_name)
+            # Video duration, in s.
+            duration = self.duration
+            # Audio bitrate, in bps.
+            audio_bitrate = self.audio_bitrate
+            # Target total bitrate, in bps.
+            target_total_bitrate = (size_upper_bound * 1024 * 8) / (1.073741824 * duration)
+            if target_total_bitrate < total_bitrate_lower_bound:
+                print('Bitrate is extremely low! Stop compress!')
+                return None
+
+            # Best min size, in kB.
+            best_min_size = (min_audio_bitrate + min_video_bitrate) * (1.073741824 * duration) / (8 * 1024)
+            if size_upper_bound < best_min_size:
+                print('Quality not good! Recommended minimum size:', '{:,}'.format(int(best_min_size)), 'KB.')
+                # return None
+
+            # Target audio bitrate, in bps.
+            audio_bitrate = audio_bitrate
+
+            # target audio bitrate, in bps
+            if 10 * audio_bitrate > target_total_bitrate:
+                audio_bitrate = target_total_bitrate / 10
+                if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+                    audio_bitrate = min_audio_bitrate
+                elif audio_bitrate > max_audio_bitrate:
+                    audio_bitrate = max_audio_bitrate
+
+            # Target video bitrate, in bps.
+            video_bitrate = target_total_bitrate - audio_bitrate
+            if video_bitrate < 1000:
+                print('Bitrate {} is extremely low! Stop compress.'.format(video_bitrate))
+                return None
+
+            i = ffmpeg.input(video_full_path)
+            if two_pass:
+                _, err = ffmpeg.output(i, os.devnull,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}
+                            ).overwrite_output().run(quiet=True)
+                logger.error('ffmpeg stderr: %s', err.decode())
+                _, err = ffmpeg.output(i, file_name,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}
+                            ).overwrite_output().run(quiet=True)
+                logger.error('ffmpeg stderr: %s', err.decode())
+                
+            else:
+                _, err = ffmpeg.output(i, file_name,
+                            **{'c:v': 'libx264', 'b:v': video_bitrate, 'c:a': 'aac', 'b:a': audio_bitrate}
+                            ).overwrite_output().run(quiet=True)
+                logger.error('ffmpeg stderr: %s', err.decode())
+
+            if os.path.getsize(file_name) <= size_upper_bound * 1024:
+                return Path(file_name)
+            elif os.path.getsize(file_name) < os.path.getsize(video_full_path):  # Do it again
+                return self.compress_video(size_upper_bound, file_name)
+            else:
+                return None
+        except FileNotFoundError as e:
+            print('You do not have ffmpeg installed!', e)
+            print('You can install ffmpeg by reading https://github.com/kkroening/ffmpeg-python/issues/251')
+            return None
+
+if __name__ == '__main__':
+    vid = Video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+    vid.compress_video(12*1000)
